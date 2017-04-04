a41fdd2ddb778bab8b2c8109496cd124b33f79d8
ricemitc <mitchrice90@gmail.com>
2017-04-04 09:53:35 -0400

    Added an activity for the metrics, and a button in the action bar to navigate to it from the main activity

--- DimIt/app/src/main/java/com/att/a56withrice/dimit/MainActivity.java
+++ DimIt/app/src/main/java/com/att/a56withrice/dimit/MainActivity.java
@@ -1,7 +1,11 @@
 package com.att.a56withrice.dimit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_metrics:
+                // User clicked the metrics button at the top
+                Intent intent = new Intent(this, metrics.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // Add the custom menu to the menu bar
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
     }
 }
---
+++ DimIt/app/src/main/java/com/att/a56withrice/dimit/metrics.java
@@ -0,0 +1,13 @@
+package com.att.a56withrice.dimit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class metrics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metrics);
+    }
+}
